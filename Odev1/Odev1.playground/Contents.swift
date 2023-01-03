116f80e8cd84912b870e8fe1519150ef6617672e
berkaytuncel <berkaytncl98@gmail.com>
2023-01-04 00:33:42 +0300

    Odev1 tamamlandi

---
+++ Odev1/Odev1.playground/Contents.swift
@@ -0,0 +1,99 @@
+import UIKit
+
+let sehir: String = "Kocaeli"
+let ulke: String = "Turkiye"
+let telefon: String = "+905313444523"
+let postaKodu: Int = 41_040
+let email: String = "berkaytncl98@gmail.com"
+let meslek: String = "Yazilimci"
+let stokMiktari: Int = 1500
+let musteriAdi: String = "Hasan"
+let bakiye: Double = 1_107_450.40
+let dogumGunu: String = "04/10/1998"
+let maas: Double = 10_000
+let medeniDurum: String = "Bekar"
+let urunYorum: String = """
+
+Urun cok guzel cok kullanisli bir urun
+                         Berkay Tuncel
+"""
+let odemeTarihi: String = "05/01/2023"
+let odeme: String = "5750$"
+let siparisAdeti: Int = 500
+let arabaModeli: String = "Civic"
+let kitapAdi: String = "Sefiller"
+let yayinlamaTarihi: String = "1862"
+let indirimMiktari: String = "%20"
+let odaSayisi: Int = 4
+let enlem: Double = 30.651399
+let boylam: Double = 41.97362
+let urunAdi: String = "Tantuni"
+let yemekFiyati: Double = 50
+let marka: String = "Casio"
+let muzikAdi: String = "Defkhan ft. Sagopa Kajmer - Onlarla Konuşuyorum"
+let videoSuresi: String = "30:41"
+let urunPuani: Float = 4.5
+let resimAdi: String = "manzara_resmi.jpg"
+let dosyaFormati: String = ".txt"
+let renk: String = "White"
+let renkKodu: String = "#FFFFFF"
+let telefonModeli: String = "iPhone 14 Pro Max"
+let ekranBoyutu: String = "6.7 inç"
+let agirlik: String = "90 kg"
+let ulusalGun: String = "23 Nisan"
+let tatilGunu: String = "Pazar"
+let rezervasyonTarihi: String = "05/01/2023"
+let sokakAdi: String = "Koru"
+let otobusHatti: String = "98T"
+let kalanDakika: Int = 50
+let takipKodu: String = "2768943088152"
+let kuponSuresi: String = "03/01/2023 - 05/01/2023"
+let kuponKodu: String = "YEMEK50"
+let faturaAdresi: String = "Yahyakaptan Mah. Kocaeli/Izmit"
+
+print("Sehir : \(sehir)")
+print("Ulke : \(ulke)")
+print("Telefon : \(telefon)")
+print("Posta Kodu : \(postaKodu)")
+print("Email : \(email)")
+print("Meslek : \(meslek)")
+print("Stok Miktari : \(stokMiktari)")
+print("Musteri Adi : \(musteriAdi)")
+print("Bakiye : \(bakiye)")
+print("Dogum Gunu : \(dogumGunu)")
+print("Maas : \(maas)")
+print("Medeni Durum : \(medeniDurum)")
+print("Urun Yorum : \(urunYorum)")
+print("Odeme Tarihi : \(odemeTarihi)")
+print("Odeme : \(odeme)")
+print("Siparis Adeti : \(siparisAdeti)")
+print("Araba Modeli : \(arabaModeli)")
+print("Kitap Adi : \(kitapAdi)")
+print("Yayinlama Tarihi : \(yayinlamaTarihi)")
+print("Indirim Miktari : \(indirimMiktari)")
+print("Oda Sayisi : \(odaSayisi)")
+print("Enlem : \(enlem)")
+print("Boylam : \(boylam)")
+print("Urun Adi : \(urunAdi)")
+print("Yemek Fiyati : \(yemekFiyati)")
+print("Marka : \(marka)")
+print("Muzik Adi : \(muzikAdi)")
+print("Video Suresi : \(videoSuresi)")
+print("Urun Puani : \(urunPuani)")
+print("Resim Adi : \(resimAdi)")
+print("Dosya Formati : \(dosyaFormati)")
+print("Renk : \(renk)")
+print("Renk Kodu : \(renkKodu)")
+print("Telefon Modeli : \(telefonModeli)")
+print("Ekran Boyutu : \(ekranBoyutu)")
+print("Agirlik : \(agirlik)")
+print("Ulusal Gun : \(ulusalGun)")
+print("Tatil Gunu : \(tatilGunu)")
+print("Rezervasyon Tarihi : \(rezervasyonTarihi)")
+print("Sokak Adi : \(sokakAdi)")
+print("Otobus Hatti : \(otobusHatti)")
+print("Kalan Dakika : \(kalanDakika)")
+print("Takip Kodu : \(takipKodu)")
+print("Kupon Suresi : \(kuponSuresi)")
+print("Kupon Kodu : \(kuponKodu)")
+print("Fatura Adresi : \(faturaAdresi)")
